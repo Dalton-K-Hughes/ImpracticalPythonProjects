da47aafbe6bed24a6e1c6b2c8fdd3e332e72ed62
Dalton Hughes <hughesda@lee.k12.ga.us>
2024-09-16 12:48:21 -0400

    Added code for loading a text file as a list of strings in load_dictionary.py from the book

---
+++ Ch2_FindingPalingramSpells/load_dictionary.py
@@ -0,0 +1,24 @@
+"""Load a text file as a list
+
+Arguments:
+-IOError if filename not found
+
+Returns:
+-A list of all words in a text file in lower case
+
+Requires-import sys
+"""
+
+import sys
+
+def load(file):
+    """Open a text file and return a list of lowercase strings."""
+    try:
+        with open(file) as in_file:
+            loaded_txt = in_file.read().strip().split('\n')
+            loaded_txt = [x.lower() for x in loaded_txt]
+            return loaded_txt
+    except IOError as e:
+        print("{}\nError Opening {}. Terminating Program.".format(e, file), file=sys.stderror)
+        sys.exit(1)
+
